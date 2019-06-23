bec3b1e9da72ea237188c0df806de2d6c1d8f0ae
Angela Cullen <angela@hubblehq.com>
2019-06-23 13:36:44 +0100

    Replaced basic config object with react context

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,6 +1,4 @@
 export { default as Grid } from './Grid';
 export { default as Row } from './Row';
 export { default as Column } from './Column';
-export { default as col } from './col';
-export { default as columnWidth } from './columnWidth';
-export { default as responsiveWidth } from './responsiveWidth';
+export { default as GridConfigProvider } from './GridConfig';
